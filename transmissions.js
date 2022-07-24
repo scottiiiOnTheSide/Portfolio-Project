882f94eb69a4aed95b5209f11eef227af8bfd3f5
beammeupscottiii <via.my.media@gmail.com>
2022-07-23 21:53:57 -0400

    More progress in transmissions section. see notes.txt

---
+++ transmissions.js
@@ -0,0 +1,52 @@
+
+const optionsWrapper = document.getElementById('optionsWrapper'),
+	  options = Array.from(document.getElementsByClassName('opt')),
+	  socials = document.getElementById('socials');
+
+
+let closeWrapper = () => {
+		setTimeout(() => {
+			optionsWrapper.style.width = "222px";
+			optionsWrapper.style.height = '225px';
+
+			options.forEach(element => {
+				element.style.opacity = 0;
+				setTimeout(() => {
+					element.style.display = 'none';
+				}, 310)
+			})
+		}, 500)
+	},
+	openWrapper_socials = () => {
+		optionsWrapper.style.transform = "translate(-50%, -50%) scale(0.3)";
+		socials.style.transform = 'translate(-50%, -50%) scale(0.5)'
+
+		setTimeout(() => {
+			if(window.innerWidth < 440) {
+				optionsWrapper.style.width = "301%";
+			} else {
+				optionsWrapper.style.width = "167%";
+			}
+
+			
+		}, 600)
+		setTimeout(() => {
+			socials.style.display = "block";
+		}, 610)
+		setTimeout(() => {
+			socials.style.opacity = 1;
+			socials.style.transform = 'translate(-50%, -50%) scale(1)'
+		}, 725)
+	};
+
+
+options[1].addEventListener('click', ()=> {
+	closeWrapper();
+	setTimeout(()=>{
+		openWrapper_socials()
+	}, 1500);
+})
+
+// optionsWrapper.style.transform = "translate(-50%, -50%) scale(0.3)";
+// optionsWrapper.style.width = "301%" max440
+// optionsWrapper.style.width = "167%" min441
